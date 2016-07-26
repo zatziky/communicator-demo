2a5e86474648dace648ed0fc69f1a9ac12ef72e3
Matouš Kučera <matous.kucera@gmail.com>
2016-07-26 14:11:03 +0200

    use double quotes again

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -9,11 +9,11 @@ aws configure set default.region eu-central-1
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=elasticbeanstalk-eu-central-2-communicator-demo-config
-APP_NAME='Leafdock Communicator Demo'
+APP_NAME="Leafdock Communicator Demo"
 ENV_NAME=leafdockCommunicatorDemo-env
 DOCKERRUN_FILE=Dockerrun_$SHA1.aws.json
-echo 'Saving new docker file to S3: $DOCKERRUN_FILE'
-sed -e 's/<TAG>/$SHA1/' -e 's/<EB_BUCKET>/$EB_BUCKET/' aws/Dockerrun.aws.json.template > $DOCKERRUN_FILE
+echo "Saving new docker file to S3: $DOCKERRUN_FILE"
+sed -e "s/<TAG>/$SHA1/" aws/Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name "$APP_NAME" \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE

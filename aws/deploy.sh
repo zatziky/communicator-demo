74bcbedb9b9fc50f337dc8a54caa2fe90cf0558f
Matouš Kučera <matous.kucera@gmail.com>
2016-07-26 12:24:48 +0200

    circle ci build and auto deploy to aws

---
+++ aws/deploy.sh
@@ -0,0 +1,23 @@
+# deploy.sh
+#! /bin/bash
+
+SHA1=$1
+
+# Configure aws (aws_access_key_id and aws_secret_access_key are set on circle-ci server)
+aws --version
+aws configure set default.region eu-central-1
+
+# Create new Elastic Beanstalk version
+EB_BUCKET=elasticbeanstalk-eu-central-2-communicator-demo-config
+APP_NAME="Leafdock Communicator Demo"
+ENV_NAME=leafdockCommunicatorDemo-env
+DOCKERRUN_FILE=Dockerrun_$SHA1.aws.json
+echo "Saving new docker file to S3: $DOCKERRUN_FILE"
+sed -e "s/<TAG>/$SHA1/" -e "s/<EB_BUCKET>/$EB_BUCKET/" docker/Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name $APP_NAME \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name $ENV_NAME \
+    --version-label $SHA1
